75e1f2658461ab00278963e4ca0ed03e3c7b1a1c
AlexGett <alex_gettinger@yahoo.com>
2025-06-09 15:17:02 +0200

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'schichtkalender-v1.3'; // Erhöhe die Versionsnummer bei Änderungen
+const CACHE_NAME = 'schichtkalender-v1.3'; // Versionsnummer erhöht
 const urlsToCache = [
     './',
     './index.html',
